bba1829abfad9edc7754b6c1aeb80914fd78c21a
Carbowix <killeryoyo01@gmail.com>
2023-02-21 20:47:57 +0200

    tab icon

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
+  build: {
+    assetsDir: 'static',
+    brotliSize: false, // not supported in StackBlitz
+  },
 })
